b375027fca2ca5e7fb9f15a7d35633b42fe1d1d1
jsdevel <js.developer.undefined@gmail.com>
2015-12-11 11:00:33 -0700

    Allowing handler to pass error back to callback.

--- err-handler.js
+++ err-handler.js
@@ -5,8 +5,12 @@ module.exports = errHandler;
 function errHandler(handler, cb){
   return function(){
     var args = [].slice.call(arguments);
-    var err = args.shift();
-    if(err)return handler(err);
+    var err = args[0];
+
+    if (err && !handler(err)) {
+      return;
+    }
+
     cb.apply(null, args);
   };
 }
--- test.js
+++ test.js
@@ -1,17 +1,36 @@
 'use strict';
 
+var sinon = require('sinon');
+
 describe('err-handler', function(){
   var errHandler = require('./');
   var sinon = require('sinon');
-  
-  it('passes errors to handler', function(){
-    var handler = sinon.stub();
-    errHandler(handler, function(){})(5);
-    sinon.assert.calledWith(handler, 5);
+
+  describe('when an error occurs', function() {
+    it('should pass errors to handler', function(){
+      var err = new Error();
+      var handler = sinon.stub();
+      errHandler(handler, function(){})(err);
+      sinon.assert.calledWith(handler, err);
+    });
+
+    describe('when the handler does not return undefined', function() {
+      it('should pass the error back to the callback', function() {
+        var err = new Error();
+        var handler = sinon.stub().returns(err);
+        var callback = sinon.stub();
+        errHandler(handler, callback)(err);
+        sinon.assert.calledWith(callback, err);
+      });
+    });
   });
 
-  it('calls cb when no error', function(done){
-    var handler = sinon.stub();
-    errHandler(handler, done)(null);
+  describe('when no error occurs', function() {
+    it('calls cb with arguments', function(){
+      var handler = sinon.stub();
+      var callback = sinon.stub();
+      errHandler(handler, callback)(null, 5);
+      sinon.assert.calledWith(callback, null, 5);
+    });
   });
 });
